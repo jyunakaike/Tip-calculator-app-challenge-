77d1a04b3dfcc7750f1863d64442a8bcb2cec5d3
jyun <jyunakaike@gmail.com>
2021-12-20 14:26:43 -0400

    scripts f

--- script.js
+++ script.js
@@ -1,53 +1,49 @@
+var bill = document.querySelector(".bill");
+var tip = document.getElementsByClassName("percentage");
+var customTip = document.querySelector(".custom");
+var people = document.querySelector(".peopleInput");
+
 var tipAmount = document.querySelector(".tipAmount");
 var total = document.querySelector(".total");
-var tip = document.getElementsByClassName("percentage");
-var bill = document.querySelector(".bill");
-var people = document.querySelector(".peopleInput");
+
+let percentageValue = 0;
 var buttonReset = document.getElementsByClassName("button");
-var customTip = document.querySelector(".custom");
-// var resetBtn = document.querySelector(".reset");
 
-
-// console.log(tipAmount);
-// console.log(total);
-// console.log(tip);
-// console.log(tip[0].value);
-// console.log(tip[1].value);
-// console.log(tip[2].value);
-// console.log(tip[3].value);
-// console.log(tip[4].value);
-// console.log(bill.value);
-// console.log(people.value);
-// console.log(buttonReset);
-// console.log(customTip);
-
-
-// for(var i=0 ; i<tip.length ; i++ ){
-//     tip[i].addEventListener("click", ()=>{
-//         var current = document.getElementsByClassName("active");
-
-//         if(current.length >0){
-//             current[0].className = cur
-//         }
-//     })
-// }
-
-let percentageValue= 0;
-
-function percentageClick(value){
+function percentageClick(value) {
+    console.log(value)
     percentageValue = value;
-    console.log(percentageValue);
-    console.log(bill.value);
-    console.log(people.value);
-    calculate(percentageValue, bill.value, people.value);
+    calculate();
     
 }
 
-function calculate(percentage, bill, people){
-    let AmountTip = (bill * (percentage/100))/people;
-    let total = AmountTip + bill / people
-    console.log( AmountTip );
-    console.log(total);
+function calculate() {
+    if (bill.value > 0 && people.value > 0 && percentageValue != null) {
+        let AmountTip = (bill.value * (percentageValue / 100)) ;
+        let totals = parseFloat(AmountTip)  + parseFloat(bill.value) ;
+        console.log(AmountTip);
+        console.log(totals);
+        tipAmount.innerHTML = "$" + (AmountTip/people.value).toFixed(2);
+        total.innerHTML = "$" + (totals/people.value).toFixed(2);
+    }
+
 }
 
+const resetButton= ()=>{
+    people.value = '';
+    bill.value = '';
+    tipAmount.innerHTML = '$' + "0.00";
+    total.innerHTML= '$' + "0.00";
+    customTip.value='';
+}
 
+bill.addEventListener("change",function () {
+    calculate();
+})
+people.addEventListener("change",function () {
+    calculate();
+})
+
+
+// bill.addEventListener("change", calculate);
+// people.addEventListener("change",calculate);
+// customTip
